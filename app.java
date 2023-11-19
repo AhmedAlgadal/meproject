8b8526c78a8bdf793520a6cccf29949e598e91e0
AhmedAli <ahmedalialgadal77@gmail.com>
2023-11-19 10:29:53 +0300

    Revert "undo pushed commit"
    This reverts commit 7c710be89bd23a49c517018e43152f09be1cdc23.

--- app.java
+++ app.java
@@ -2,7 +2,6 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("dhfsdfds");
-        System.out.println("hfd");
 
     }
 }
