7c710be89bd23a49c517018e43152f09be1cdc23
AhmedAli <ahmedalialgadal77@gmail.com>
2023-11-19 10:16:26 +0300

    undo pushed commit

--- app.java
+++ app.java
@@ -2,6 +2,7 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("dhfsdfds");
+        System.out.println("hfd");
 
     }
 }
